6c720508695f9790b30e0493e76c43b8f9d8ddae
Michał Pasynek <terabajt@terabajtlw.pl>
2023-09-08 14:42:44 +0200

    [Feature] Add logout option.

--- src/app/user-reimbursement/user-reimbursement-menu/user-reimbursement-menu.component.ts
+++ src/app/user-reimbursement/user-reimbursement-menu/user-reimbursement-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { faCoffee, faHouse, faPlus, faPrint, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
+import { AuthService } from "src/app/auth/auth.service";
 
 @Component({
 	selector: "app-user-reimbursement-menu",
@@ -7,8 +8,13 @@ import { faCoffee, faHouse, faPlus, faPrint, faRightToBracket } from "@fortaweso
 	styleUrls: ["./user-reimbursement-menu.component.css"],
 })
 export class UserReimbursementMenuComponent {
+	constructor(private authService: AuthService) {}
 	faHouse = faHouse;
 	faPlus = faPlus;
 	faPrint = faPrint;
 	faRightToBracket = faRightToBracket;
+
+	logout() {
+		this.authService.logout();
+	}
 }
